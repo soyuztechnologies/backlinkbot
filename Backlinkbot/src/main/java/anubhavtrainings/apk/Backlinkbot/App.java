216029ffbf8deaee679ebf0e87d4e59957f8455b
sc <sc@DESKTOP-3DNLE8Q>
2020-06-05 16:07:44 +0530

    youtube comment

--- Backlinkbot/src/main/java/anubhavtrainings/apk/Backlinkbot/App.java
+++ Backlinkbot/src/main/java/anubhavtrainings/apk/Backlinkbot/App.java
@@ -7,11 +7,11 @@ import java.io.IOException;
 public class App {
   public static void main(String[] args) throws InterruptedException, IOException {
 	  Backlink c = new Backlink();
-	  ((Backlink) c).myMethod1();
-	  ((Backlink) c).myMethod();
-	  ((Backlink) c).myMethod2();
-	  ((Backlink) c).myMethod3();
-	  ((Backlink) c).myMethod4();
+	  //((Backlink) c).myMethod1();
+	  //((Backlink) c).myMethod();
+	  //((Backlink) c).myMethod2();
+	  //((Backlink) c).myMethod3();
+	  ((Backlink) c).myMethod5();
 	//PinSites p = new PinSites();
     //((PinSites) p).mypin();
 	  
